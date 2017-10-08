716f74dcab6637f26c031ee7ad21c5fb1090e9be
stoias <pddybrd.s@gmail.com>
2017-10-08 21:18:01 +0900

    地図アプリselect追加

--- app/src/main/java/com/example/mapapp01/MapsActivity.java
+++ app/src/main/java/com/example/mapapp01/MapsActivity.java
@@ -42,7 +42,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
+        mMap = googleMap; 
 
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
