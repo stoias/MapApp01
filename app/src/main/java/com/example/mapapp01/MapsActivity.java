ee7a86aa6be2c07326fff1c4600abe71ac840541
stoias <pddybrd.s@gmail.com>
2017-11-01 21:25:35 +0900

    地図アプリ　マーカー表示

--- app/src/main/java/com/example/mapapp01/MapsActivity.java
+++ app/src/main/java/com/example/mapapp01/MapsActivity.java
@@ -1,9 +1,11 @@
 package com.example.mapapp01;
 
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -24,10 +26,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-        // DBオープン処理
-        DatabaseHelper dbHelper = new DatabaseHelper(this);
-        db = dbHelper.getWritableDatabase();
-
     }
 
 
@@ -42,11 +40,33 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap; 
+        mMap = googleMap;
+        
+        // DBオープン処理
+        DatabaseHelper dbHelper = new DatabaseHelper(this);
+        db = dbHelper.getWritableDatabase();
+        //SQL文
+        String sql    = "SELECT _id,name,longitude,latitude,memo FROM Map";
+        //SQL文の実行
+        Cursor cursor = db.rawQuery(sql,null);
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        while(cursor.moveToNext()){
+            int index_id = cursor.getColumnIndex("_id");
+            int index_name = cursor.getColumnIndex("name");
+            int index_longitude = cursor.getColumnIndex("longitude");
+            int index_latitude = cursor.getColumnIndex("latitude");
+            int index_memo = cursor.getColumnIndex("memo");
+            int id = cursor.getInt(index_id);
+            String name = cursor.getString(index_name);
+            String memo = cursor.getString(index_memo);
+            Double longitude = cursor.getDouble(index_longitude);
+            Double latitude = cursor.getDouble(index_latitude);
+
+            LatLng point = new LatLng(longitude, latitude);
+            mMap.addMarker(new MarkerOptions().position(point).title(name));
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(point));
+        }
     }
+
+
 }
